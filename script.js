675671aed197195603f657b829793f397b4374f5
yuube <yuubemade@gmail.com>
2018-08-20 13:35:46 -0400

    Delete gh-pages branch; add extra conditions to golden card usage.

--- script.js
+++ script.js
@@ -1,37 +1,59 @@
 window.onload = function(){
-  var sound1 = new Howl({
+  var goldSound = new Howl({
     src: ['assets/golden.ogg']
   });
-  var sound2 = new Howl({
+  var regSound = new Howl({
     src: ['assets/regular.ogg']
   });
+
+  var first = true
+  var wasGolden = false
   
   var body = document.querySelector('#main');
   body.addEventListener("click", function(evt){
-    const random = Math.round(Math.random() * 10 + 1);
-    console.log(random)
+    const random = Math.round(Math.random() * 10);
     var golden = false;
 
-    if (random > 8) {
+    if (random > 7) {
       golden = true;
     }
 
     var x = evt.clientX, y = evt.clientY;
+
     img = document.createElement("img");
     img.className = "card";
-    if (golden) {
-        img.src = "assets/golden.gif";
-        sound1.play();
-    } else {
-        img.src = "assets/regular.png";
-        sound2.play();
-    };
     img.style.top = y - 197 + "px",
     img.style.left = x - 143 + "px";
     img.draggable = false,     
     img.ondragstart = function() {
       return false;
     }
+
+    const useGolden = function() {
+      img.src = 'assets/golden.gif'
+      wasGolden = true
+      goldSound.play()
+      console.log('Golden (' + random.toString() + ')')
+    }
+
+    const useRegular = function() {
+      img.src = 'assets/regular.png'
+      wasGolden = false
+      regSound.play()
+      console.log('Regular (' + random.toString() + ')')
+    }
+
+    if (first) {
+        useRegular()
+        first = false
+    } else if (wasGolden) {
+        useRegular()
+    } else if (golden) {
+        useGolden()
+    } else {
+        useRegular()
+    };
+
     body.appendChild(img);
   });
 };
